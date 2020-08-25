9e7c5b69442f4869fd5ec019ebad15948489a668
alvesvictor@hotmail.com <alvesvictor-@hotmail.com>
2020-08-25 09:58:42 -0300

    Desafio 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const[repositories, setRepositories] = useState([])
+
+  useEffect(()=>{
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  },[])
+
+
   async function handleAddRepository() {
-    // TODO
+      const response = await api.post('repositories', {
+        title: 'Victor', url: 'http://hit.com', techs: ['node', 'java'], likes: ''
+      });
+      setRepositories([...repositories, response.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(item => item.id !== id ))
   }
+      
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+          {repositories.map(item => (
+            <li>
+              {item.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+              <button onClick={() => handleRemoveRepository(item.id)}>
+                Remover
+             </button>
         </li>
+      ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
